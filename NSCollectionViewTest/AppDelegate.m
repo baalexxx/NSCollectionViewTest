3549a832e6aab554ddfc41616df52a0ce29e3e0b
Alexey Baev <ab@gamayun.sk>
2016-04-07 20:02:24 +0200

    After using resources

--- NSCollectionViewTest/AppDelegate.m
+++ NSCollectionViewTest/AppDelegate.m
@@ -8,45 +8,84 @@
 
 #import "AppDelegate.h"
 
-static const NSSize buttonSize = { 80, 35 };
-static const NSSize itemSize = { 100, 100 };
-static const NSPoint buttonOrigin = { 10, 10 };
+//static const NSSize buttonSize = { 80, 35 };
+//static const NSSize itemSize = { 100, 100 };
+//static const NSPoint buttonOrigin = { 10, 10 };
 
 #pragma mark - BVView
 
-@interface BVView : NSView
-
-@property (weak) NSButton *button;
-
-@end
-
-@implementation BVView
-@synthesize button;
-- (id)initWithFrame:(NSRect)frameRect {
-    
-    self = [super initWithFrame:(NSRect){frameRect.origin, itemSize}];
-    if (self) {
-        NSButton *newButton = [[NSButton alloc]
-                               initWithFrame:(NSRect){buttonOrigin, buttonSize}];
-        [self addSubview:newButton];
-        self.button = newButton;
-    }
-    return self;
-}
-@end
+//@interface BVView : NSView
+//
+//@property (weak) NSButton *button;
+//
+//@end
+//
+//@implementation BVView
+//@synthesize button;
+//- (id)initWithFrame:(NSRect)frameRect {
+//    
+//    self = [super initWithFrame:(NSRect){frameRect.origin, itemSize}];
+//    if (self) {
+//        NSButton *newButton = [[NSButton alloc]
+//                               initWithFrame:(NSRect){buttonOrigin, buttonSize}];
+//        [self addSubview:newButton];
+//        self.button = newButton;
+//    }
+//    return self;
+//}
+//@end
 
 #pragma mark - BVPrototype
 
 @interface BVPrototype : NSCollectionViewItem
+
+@property (strong) IBOutlet NSTextField* subTitleTextField;
+
 @end
 
 @implementation BVPrototype
+
+//- (void) awakeFromNib {
+//    
+//    NSLog(@"test");
+//    
+//}
+
 - (void)loadView {
-    [self setView:[[BVView alloc] initWithFrame:NSZeroRect]];
+    
+    NSLog(@"test");
+    self.subTitleTextField.stringValue = @"AAAAA";
 }
+
+
+- (void) setView:(NSView *)view {
+    
+    [super setView:view];
+    
+    self.subTitleTextField.stringValue = @"QQQ";
+    if (self.representedObject)
+        self.subTitleTextField.stringValue = self.representedObject;
+}
+
 - (void)setRepresentedObject:(id)representedObject {
     [super setRepresentedObject:representedObject];
-    [[(BVView *)[self view] button] setTitle:representedObject];
+//    [[(BVView *)[self view] button] setTitle:representedObject];
+    
+    //self.textField.stringValue = representedObject;
+    
+//    NSLog([representedObject description]);
+//    
+//    NSLog([self.imageView description]);
+    
+    
+    if (representedObject) {
+        self.textField.stringValue = representedObject;
+        
+        self.subTitleTextField.stringValue = representedObject;
+        
+        self.subTitleTextField.stringValue = @"QQQ";
+    }
+    
 }
 @end
 
@@ -57,6 +96,7 @@ static const NSPoint buttonOrigin = { 10, 10 };
 @property (strong) NSArray *titles;
 
 @property (weak) IBOutlet NSCollectionView *collectionView;
+
 @property (weak) IBOutlet NSWindow *window;
 
 @end
@@ -66,9 +106,9 @@ static const NSPoint buttonOrigin = { 10, 10 };
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
     
     self.titles = @[@"Case", @"Molly", @"Armitage", @"Hideo", @"The Finn", @"Maelcum", @"Wintermute", @"Neuromancer"];
-    
-    [self.collectionView setItemPrototype:[BVPrototype new]];
+
     [self.collectionView setContent:[self titles]];
+    
 }
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
